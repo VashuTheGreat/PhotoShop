c34fe4cacadce94cbee56eda6541456cfbebb0e8
Vansh Sharma <vanshsharma7832@gmail.com>
2026-03-09 17:23:10 +0530

    Refactor app import and server configuration
    Refactor app import and update server run configuration to use environment variable for port.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.app import *
+from src.app import app
 from src.logger import *
 import uvicorn as uv
 from dotenv import load_dotenv
@@ -8,4 +8,10 @@ from fastapi.staticfiles import StaticFiles
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
-    uv.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=port
+    )
